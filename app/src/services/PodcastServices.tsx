6ee67b50658ed46e571ddb1fa3b3879ee4bf8d15
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2023-04-19 23:44:49 +0900

    delete createUser and create createPodcast

--- app/src/services/PodcastServices.tsx
+++ app/src/services/PodcastServices.tsx
@@ -42,11 +42,14 @@ export class PodcastServices {
     return podcasts;
   }
 
-  public async CreateUser() {
-    const user = await this.prisma.user.create({
+  public async CreatePodcast() {
+    const podcast = await this.prisma.podcast.create({
       data: {
-        email: "elsa@prisma.io",
-        name: "Elsa Prisma",
+        name: "バンクーバーのえんじに屋",
+        indexName: "vancouver-engineers",
+        imageUrl:
+          "https://content.production.cdn.art19.com/images/21/29/55/8c/2129558c-b1a7-4d6c-8ebe-9306ff147b76/bab81362790830baf24d2c8ef5055984a16f5b60d6227d9e2f834968bcd3f326b9a3d34a7f9d32150bd08cd9b0ad924c3de016e0d4203c34a1d9b8af2477d112.jpeg",
+        createdAt: "2020-10-19T00:00:00Z",
       },
     });
   }
