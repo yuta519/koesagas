4ab87e3159ce529d92aabbe22ad78412246c2187
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2023-04-20 17:44:12 +0900

    add external link to fetch image

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ['content.production.cdn.art19.com'],
+  },
 }
 
 module.exports = nextConfig
