7ba154bd172f8732857e83def0219e6190f7d08b
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2023-04-26 00:05:52 +0900

    add static image url

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['content.production.cdn.art19.com'],
+    domains: ['content.production.cdn.art19.com','podcastaddict.com'],
   },
 }
 
