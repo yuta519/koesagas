a2a3c3c6a961ebd28292f38c9b9f26cc6e70ae6f
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2023-04-25 10:48:43 +0900

    create the page to show all episodes

---
+++ frontend/src/pages/podcasts/index.tsx
@@ -0,0 +1,53 @@
+import { useEffect, useState } from "react";
+import Image from "next/image";
+import Link from "next/link";
+
+import { FetchAllPodcasts } from "@/components/features/Podcasts";
+import { Podcast } from "@/types/Podcast";
+
+const Podcasts = () => {
+  const [state, update] = useState<{ podcasts: Podcast[] }>({ podcasts: [] });
+
+  useEffect(() => {
+    (async () => {
+      const podcasts: Podcast[] = await FetchAllPodcasts();
+      update((prev) => ({ ...prev, podcasts }));
+    })();
+  }, []);
+
+  console.log(state.podcasts || []);
+
+  return (
+    <>
+      <div className="sm:mx-auto sm:w-full sm:max-w-md">
+        <h1 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
+          Podcasts
+        </h1>
+      </div>
+      <div className="grid grid-cols-3 gap-4">
+        {state.podcasts
+          ? state.podcasts.map((podcast) => (
+              <Link key={podcast.id} href={`podcasts/${podcast.id}`}>
+                <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
+                  <div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10 container">
+                    <h2 className="text-lg font-medium text-gray-800 mb-4">
+                      {podcast.name}
+                    </h2>
+                    <Image
+                      className="mx-auto"
+                      src={podcast.imageUrl}
+                      alt=""
+                      width="300"
+                      height="300"
+                    />
+                  </div>
+                </div>
+              </Link>
+            ))
+          : null}
+      </div>
+    </>
+  );
+};
+
+export default Podcasts;
