c9d86a792bde624ead421155274dec768cd5dc9b
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2023-04-25 22:05:55 +0900

    add prisma/seed data of jsparty

--- app/prisma/seed.ts
+++ app/prisma/seed.ts
@@ -2,13 +2,23 @@ import { PrismaClient } from '@prisma/client'
 const prisma = new PrismaClient()
 
 const main = async () => {
-  const podcast = await prisma.podcast.create({
-    data: {
-      name: 'バンクーバーのえんじに屋',
-      indexName: 'vancouver-engineers',
-      imageUrl: 'https://content.production.cdn.art19.com/images/21/29/55/8c/2129558c-b1a7-4d6c-8ebe-9306ff147b76/bab81362790830baf24d2c8ef5055984a16f5b60d6227d9e2f834968bcd3f326b9a3d34a7f9d32150bd08cd9b0ad924c3de016e0d4203c34a1d9b8af2477d112.jpeg',
-      createdAt: '2020-10-19T00:00:00Z',
-    }
+  const podcast = await prisma.podcast.createMany({
+    data: [
+      {
+        name: 'バンクーバーのえんじに屋',
+        indexName: 'vancouver-engineers',
+        imageUrl: 'https://content.production.cdn.art19.com/images/21/29/55/8c/2129558c-b1a7-4d6c-8ebe-9306ff147b76/bab81362790830baf24d2c8ef5055984a16f5b60d6227d9e2f834968bcd3f326b9a3d34a7f9d32150bd08cd9b0ad924c3de016e0d4203c34a1d9b8af2477d112.jpeg',
+        createdAt: '2020-10-19T00:00:00Z',
+      },
+      {
+        name: 'JS Party: JavaScript, CSS, Web Development',
+        indexName: 'js-party',
+        imageUrl: 'https://podcastaddict.com/cache/artwork/thumb/1927958',
+        createdAt: '2017-02-24T00:00:00Z',
+      },
+    ]
+
+
   });
 }
 
