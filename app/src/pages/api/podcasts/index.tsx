2bc8292dbaa77dc6e4e0d2f5d44bb413c62c4d77
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2023-04-25 10:49:43 +0900

    Add cors

--- app/src/pages/api/podcasts/index.tsx
+++ app/src/pages/api/podcasts/index.tsx
@@ -1,15 +1,22 @@
 import type { NextApiRequest, NextApiResponse } from "next";
+import Cors from "cors";
 
+import { runMiddleware } from "@/middleware";
+import { Podcast } from "@prisma/client";
 import { PodcastServices } from "@/services/PodcastServices";
 
-type Data = { name: string };
+type Data = { podcasts: Podcast[] };
+
+const cors = Cors({
+  methods: ["POST", "GET", "HEAD"],
+});
 
 const handler = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
+  await runMiddleware(req, res, cors);
   const podcastServices = new PodcastServices();
   const podcasts = await podcastServices.FetchPodcasts();
-  console.log(podcasts);
 
-  res.status(200).json({ name: "John Doe" });
+  res.status(200).json({ podcasts });
 };
 
 export default handler;
