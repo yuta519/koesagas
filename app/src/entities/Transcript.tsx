ac9b3904b49ed7029aebd3e25f4146d6b32df335
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2023-04-27 08:42:35 +0900

    add episodeId and improve sec at Transcript entity

--- app/src/entities/Transcript.tsx
+++ app/src/entities/Transcript.tsx
@@ -1,5 +1,6 @@
 export class Transcript {
   private id: string;
+  private episodeId: string;
   private rawText: string;
   private highlightText: string;
   private startAt: number;
@@ -9,24 +10,26 @@ export class Transcript {
 
   constructor({
     id,
+    episodeId,
     rawText,
     highlightText,
     startAt,
     endAt,
   }: {
     id: string;
+    episodeId: string;
     rawText: string;
     highlightText: string;
     startAt: number;
     endAt: number;
   }) {
     this.id = id;
+    this.episodeId = episodeId;
     this.rawText = rawText;
     this.highlightText = highlightText.replace(
       /<em>/g,
       '<em class="highlight">'
     );
-
     this.startAt = startAt;
     this.endAt = endAt;
     this.formatedStartAt = this.formatTime(startAt);
@@ -36,7 +39,7 @@ export class Transcript {
   private formatTime(time: number): string {
     const hour = Math.floor(time / 3600);
     const min = Math.floor((time % 3600) / 60);
-    const sec = time % 60;
+    const sec = Math.floor(time % 60);
 
     return `${hour}:${min}:${sec}`;
   }
