57e9c98b5c3c2e34167a6cd74bc7d7f6c055a019
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2023-04-28 17:21:22 +0900

    enable enter to submit

--- frontend/src/components/ui/SearchBox/index.tsx
+++ frontend/src/components/ui/SearchBox/index.tsx
@@ -9,23 +9,30 @@ export const SearchBox = ({
   searchText: string;
   onClick: () => void;
 }) => {
+  // TODO: any
+  const handleClick = (event: any) => {
+    event.preventDefault();
+    onClick();
+  };
   return (
     <>
-      <div className="flex border border-purple-200 rounded">
-        <input
-          type="text"
-          className="block w-full px-4 py-2 text-purple-700 bg-white border rounded-md focus:border-purple-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40"
-          placeholder="Search..."
-          value={searchText}
-          onChange={onChange}
-        />
-        <button
-          className="px-4 text-white bg-gray-600 border-l rounded "
-          onClick={onClick}
-        >
-          Search
-        </button>
-      </div>
+      <form onSubmit={handleClick}>
+        <div className="flex border border-purple-200 rounded">
+          <input
+            type="text"
+            className="block w-full px-4 py-2 text-purple-700 bg-white border rounded-md focus:border-purple-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40"
+            placeholder="Search..."
+            value={searchText}
+            onChange={onChange}
+          />
+          <button
+            className="px-4 text-white bg-gray-600 border-l rounded "
+            onClick={onClick}
+          >
+            Search
+          </button>
+        </div>
+      </form>
     </>
   );
 };
