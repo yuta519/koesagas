94c7c5b8b42917a166436964aec96721a403d951
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2023-05-01 00:44:14 +0900

    create new api endpoint

---
+++ app/src/pages/api/episodes/[episode_id].tsx
@@ -0,0 +1,24 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import Cors from "cors";
+
+import { EpisodeServices } from "@/services/EpisodeServices";
+import { Episode } from "@prisma/client";
+import { runMiddleware } from "@/middleware";
+
+type Data = { episode: Episode | null };
+
+const cors = Cors({
+  methods: ["POST", "GET", "HEAD"],
+});
+
+const handler = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
+  await runMiddleware(req, res, cors);
+  const { episode_id } = req.query;
+  console.log(episode_id);
+  const episodeServices = new EpisodeServices();
+  const episode = await episodeServices.FetchById(episode_id as string);
+
+  res.status(200).json({ episode });
+};
+
+export default handler;
