8658ac623313db579f65042f469e11c67dc5163b
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2023-04-30 21:02:28 +0900

    erase unnecesarry import

--- frontend/src/pages/episodes/[episode_id].tsx
+++ frontend/src/pages/episodes/[episode_id].tsx
@@ -1,44 +1,20 @@
-import { ChangeEvent, useCallback, useState } from "react";
+import { useEffect, useState } from "react";
 
 import { Search } from "@/components/features/Search";
 import { SearchBox } from "@/components/ui/SearchBox";
 
-export interface Transcript {
-  id: string;
-  rawText: string;
-  highlightText: string;
-  startAt: number;
-  endAt: number;
-  formatedStartAt: string;
-  formatedEndAt: string;
-}
-
 const Episode = () => {
   const [state, update] = useState<{
     searchText: string;
-    hits: Transcript[];
   }>({
     searchText: "",
-    hits: [],
   });
 
-  const handleSearchBoxChange = useCallback(
-    (event: ChangeEvent<HTMLInputElement>) => {
-      update((prev) => ({ ...prev, searchText: event.target.value }));
-    },
-    []
-  );
-
-  const handleSearchBoxClick = async () => {
-    const hits = await Search("vancouver-engineers", state.searchText, "all");
-    const sortedHits = hits.sort(
-      (x: Transcript, y: Transcript) => x.startAt - y.startAt
-    );
-    update((prev) => ({
-      ...prev,
-      hits: sortedHits as Transcript[],
-    }));
-  };
+  useEffect(() => {
+    (async () => {
+      const hits = await Search("vancouver-engineers", "hello", "all");
+    })();
+  }, []);
 
   return (
     <>
@@ -47,30 +23,6 @@ const Episode = () => {
           Search transcription!
         </h1>
       </div>
-      <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-2xl">
-        <div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
-          <SearchBox
-            onChange={handleSearchBoxChange}
-            searchText={state.searchText}
-            onClick={handleSearchBoxClick}
-          />
-        </div>
-      </div>
-      {state.hits.length
-        ? state.hits.map((hit) => (
-            <div
-              key={hit.id}
-              className="mt-8 sm:mx-auto sm:w-full sm:max-w-2xl"
-            >
-              <div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
-                <p className="text-base">
-                  {hit.formatedStartAt} - {hit.formatedEndAt}
-                </p>
-                <div dangerouslySetInnerHTML={{ __html: hit.highlightText }} />
-              </div>
-            </div>
-          ))
-        : null}
     </>
   );
 };
