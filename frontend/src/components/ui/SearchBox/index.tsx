823e868c944b3f06be395829fa6182f7ca55b6fc
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2023-05-01 21:26:42 +0900

    create new components

--- frontend/src/components/ui/SearchBox/index.tsx
+++ frontend/src/components/ui/SearchBox/index.tsx
@@ -15,24 +15,26 @@ export const SearchBox = ({
     onClick();
   };
   return (
-    <>
-      <form onSubmit={handleClick}>
-        <div className="flex border border-purple-200 rounded">
-          <input
-            type="text"
-            className="block w-full px-4 py-2 text-purple-700 bg-white border rounded-md focus:border-purple-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40"
-            placeholder="Search..."
-            value={searchText}
-            onChange={onChange}
-          />
-          <button
-            className="px-4 text-white bg-gray-600 border-l rounded "
-            onClick={onClick}
-          >
-            Search
-          </button>
-        </div>
-      </form>
-    </>
+    <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-2xl">
+      <div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
+        <form onSubmit={handleClick}>
+          <div className="flex border border-purple-200 rounded">
+            <input
+              type="text"
+              className="block w-full px-4 py-2 text-purple-700 bg-white border rounded-md focus:border-purple-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40"
+              placeholder="Search..."
+              value={searchText}
+              onChange={onChange}
+            />
+            <button
+              className="px-4 text-white bg-gray-600 border-l rounded "
+              onClick={onClick}
+            >
+              Search
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
   );
 };
--- frontend/src/components/ui/Tab/index.tsx
+++ frontend/src/components/ui/Tab/index.tsx
@@ -10,16 +10,23 @@ export const Tab = ({ tabs, currentTab, onChange }: Props) => {
   const activeTab =
     "mr-2 inline-block p-4 border-b-2 rounded-t-lg text-blue-600 border-blue-600 active dark:text-blue-500 dark:border-blue-500";
 
+  const handleClick = (event: React.MouseEvent<HTMLLIElement>) => {
+    const tab = event.currentTarget.dataset.tab;
+    if (tab) {
+      onChange(tab);
+    }
+  };
+
   return (
     <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-2xl">
-      <div className="text-sm font-medium text-center text-gray-500 border-b border-gray-200 dark:text-gray-400 dark:border-gray-700">
+      <div className="text-sm font-medium text-center text-gray-500 border-b border-gray-200 dark:text-gray-400">
         <ul className="flex flex-wrap -mb-px">
           {tabs.map((tab) => (
             <li
               className={tab === currentTab ? activeTab : inactiveTab}
               key={tab}
-              value={tab}
-              onClick={() => onChange(tab)}
+              data-tab={tab}
+              onClick={handleClick}
             >
               {tab}
             </li>
