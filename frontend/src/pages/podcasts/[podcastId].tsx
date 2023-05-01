6932061bfae98b86c38cdf7c5a32d6ef57751468
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2023-05-01 21:29:05 +0900

    seperate components

--- frontend/src/pages/podcasts/[podcastId].tsx
+++ frontend/src/pages/podcasts/[podcastId].tsx
@@ -1,22 +1,27 @@
 import { ChangeEvent, useCallback, useEffect, useState } from "react";
 import { useRouter } from "next/router";
-
-import { Search } from "@/components/features/Search";
-import { SearchBox } from "@/components/ui/SearchBox";
 import { NextApiRequest } from "next";
-import { FetchPodcastById } from "@/components/features/Podcasts";
+
 import { Podcast, Transcript } from "@/types/Podcast";
-import Link from "next/link";
+import { Search } from "@/components/features/Search";
+import { FetchPodcastById } from "@/components/features/Podcasts";
+import { EpisodeList } from "@/components/features/Podcast/ui/EpisodeList";
+import { SearchTranscripts } from "@/components/features/Podcast/ui/SearchTranscripts";
+import { Tab } from "@/components/ui/Tab";
 
 const Podcast = (req: NextApiRequest) => {
   const router = useRouter();
   const { podcastId } = router.query;
-  const [state, update] = useState<{
+
+  interface State {
+    currentTab: string;
     searchText: string;
     targetEpisodeId: string;
     hits: Transcript[];
     podcast: Podcast | null;
-  }>({
+  }
+  const [state, update] = useState<State>({
+    currentTab: "Transcripts",
     searchText: "",
     targetEpisodeId: "all",
     hits: [],
@@ -32,29 +37,40 @@ const Podcast = (req: NextApiRequest) => {
     }
   }, [podcastId]);
 
-  const handleSearchBoxChange = useCallback(
+  const tabs = ["Transcripts", "Episodes"];
+
+  const sortedEpisodes = state.podcast?.episodes.sort((x, y) => {
+    return x.backnumber - y.backnumber;
+  });
+
+  const handleChangeSearchBox = useCallback(
     (event: ChangeEvent<HTMLInputElement>) => {
       update((prev) => ({ ...prev, searchText: event.target.value }));
     },
     []
   );
 
-  const sortedEpisodes = state.podcast?.episodes.sort((x, y) => {
-    return x.backnumber - y.backnumber;
-  });
+  const handleChangeTab = useCallback((tab: string) => {
+    update((prev) => ({ ...prev, currentTab: tab }));
+  }, []);
 
   // TODO: any
   const handleChangeEpisode = useCallback((event: any) => {
-    update((prev) => ({ ...prev, targetEpisodeId: event.target.value }));
+    update((prev) => ({
+      ...prev,
+      targetEpisodeId: event.target.value,
+    }));
   }, []);
 
-  const handleSearchBoxClick = useCallback(async () => {
+  const handleClickSearchBox = useCallback(async () => {
     if (state.podcast === null) return;
-
+    const targetEpisodeBacknumber = state.podcast.episodes.find((episode) => {
+      return episode.id === state.targetEpisodeId;
+    })?.backnumber;
     const hits = await Search(
       state.podcast?.indexName,
       state.searchText,
-      state.targetEpisodeId
+      targetEpisodeBacknumber || "all"
     );
     const sortedHits = hits.sort(
       (x: Transcript, y: Transcript) => x.startAt - y.startAt
@@ -75,55 +91,23 @@ const Podcast = (req: NextApiRequest) => {
           Search Transcription!
         </h2>
       </div>
-      <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-2xl">
-        <select
-          id="episodes"
-          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-          onChange={handleChangeEpisode}
-        >
-          <option defaultValue="all" value="all" selected>
-            All Episodes
-          </option>
-          {sortedEpisodes?.map((episode) => (
-            <option key={episode.id} value={episode.backnumber}>
-              {episode.backnumber} {episode.title}
-            </option>
-          ))}
-        </select>
-      </div>
-      <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-2xl">
-        <div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
-          <SearchBox
-            onChange={handleSearchBoxChange}
-            searchText={state.searchText}
-            onClick={handleSearchBoxClick}
-          />
-        </div>
-      </div>
-      {state.hits.length
-        ? state.hits.map((hit) => (
-            <div
-              key={hit.id}
-              className="mt-8 sm:mx-auto sm:w-full sm:max-w-2xl"
-            >
-              <div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
-                <p className="text-base">
-                  Episode:
-                  <Link href={`/episodes/${hit.episodeId}`}>
-                    {hit.episodeId}
-                  </Link>
-                </p>
-                <p className="text-base">
-                  Duration: {hit.formatedStartAt} - {hit.formatedEndAt}
-                </p>
-                <p
-                  className="pt-4"
-                  dangerouslySetInnerHTML={{ __html: hit.highlightText }}
-                />
-              </div>
-            </div>
-          ))
-        : null}
+      <Tab
+        tabs={tabs}
+        currentTab={state.currentTab}
+        onChange={handleChangeTab}
+      ></Tab>
+      {state.currentTab === "Transcripts" ? (
+        <SearchTranscripts
+          episodes={sortedEpisodes}
+          onChangeEpisode={handleChangeEpisode}
+          searchText={state.searchText}
+          onChangeSearchText={handleChangeSearchBox}
+          onClickSearchButton={handleClickSearchBox}
+          hitTranscripts={state.hits}
+        />
+      ) : (
+        <EpisodeList episodes={sortedEpisodes} />
+      )}
     </>
   );
 };
