4aea317b66b1c127a51844677ea7b0fd40b036da
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2023-05-02 01:26:06 +0900

    create new dir and files for const

---
+++ transcriber/const/algolia.py
@@ -0,0 +1,3 @@
+class IndexName:
+    JSPARTY = "js-party"
+    VANCOUVER_ENGINEERS = "vancouver-engineers"
---
+++ transcriber/const/whisper.py
@@ -0,0 +1,11 @@
+class Model:
+    TINY = 'tiny'
+    BASE = 'base'
+    SMALL = 'small'
+    MEDIUM = 'medium'
+    LARGE = 'large'
+
+
+class Language:
+    ENGLISH = 'English'
+    JAPAENSE = 'Japanese'
