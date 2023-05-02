9a3da7e80fa25afcc17671bf188e897567b26ff2
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2023-05-02 18:44:44 +0900

    add a button to link for episode detail

--- frontend/src/components/features/Podcast/ui/HitTranscripts.tsx
+++ frontend/src/components/features/Podcast/ui/HitTranscripts.tsx
@@ -25,15 +25,7 @@ export const HitTranscripts = ({ episodes, hits }: Props) => {
               <div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
                 <p className="text-base">
                   Episode:
-                  <Link
-                    href={`/episodes/${
-                      episodeIdAndBacknumbers?.find((obj) => {
-                        return obj[hit.episodeId];
-                      })?.[hit.episodeId]
-                    }`}
-                  >
-                    {hit.episodeId}
-                  </Link>
+                  {hit.episodeId}
                 </p>
                 <p className="text-base">
                   Duration: {hit.formatedStartAt} - {hit.formatedEndAt}
@@ -42,6 +34,19 @@ export const HitTranscripts = ({ episodes, hits }: Props) => {
                   className="pt-4"
                   dangerouslySetInnerHTML={{ __html: hit.highlightText }}
                 />
+                <div className="flex justify-center">
+                  <Link
+                    href={`/episodes/${
+                      episodeIdAndBacknumbers?.find((obj) => {
+                        return obj[hit.episodeId];
+                      })?.[hit.episodeId]
+                    }`}
+                  >
+                    <button className="mx-2 bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded">
+                      To Episode
+                    </button>
+                  </Link>
+                </div>
               </div>
             </div>
           ))
