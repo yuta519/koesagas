192f7c5e4f36a18044df563e77deb638c6d1cbae
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2023-05-02 17:03:40 +0900

    add link to each episode on episode page

--- frontend/src/components/features/Podcast/ui/EpisodeList.tsx
+++ frontend/src/components/features/Podcast/ui/EpisodeList.tsx
@@ -1,4 +1,5 @@
 import { Episode } from "@/types/Podcast";
+import Link from "next/link";
 import React from "react";
 
 interface Props {
@@ -10,16 +11,15 @@ export const EpisodeList = ({ episodes }: Props) => {
     <>
       {episodes?.length
         ? episodes.map((episode) => (
-            <div
-              key={episode.id}
-              className="mt-8 sm:mx-auto sm:w-full sm:max-w-2xl"
-            >
-              <div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
-                <div className="text-base">Episode: {episode.backnumber}</div>
-                <div className="text-base">Title: {episode.title}</div>
-                <div className="pt-4">{episode.description}</div>
+            <Link key={episode.id} href={`/episodes/${episode.id}`}>
+              <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-2xl">
+                <div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
+                  <div className="text-base">Episode: {episode.backnumber}</div>
+                  <div className="text-base">{episode.title}</div>
+                  <div className="pt-4">{episode.description}</div>
+                </div>
               </div>
-            </div>
+            </Link>
           ))
         : null}
     </>
