a066cbc315ce343f94a41282bc1f13b58f7de348
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2023-05-05 22:48:28 +0900

    erase unnecessary import

--- app/src/services/PodcastServices.tsx
+++ app/src/services/PodcastServices.tsx
@@ -1,4 +1,4 @@
-import { Prisma, PrismaClient } from "@prisma/client";
+import { PrismaClient } from "@prisma/client";
 import { AlgoriaClient } from "@/infras/AlgoriaClient";
 import { Transcript } from "@/entities/Transcript";
 
