d2c698a4b9f20d58431e6aedc6a91746708eabb0
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2023-05-05 18:51:11 +0900

    Change API domain from localhost to env vailables

--- frontend/src/components/features/Search.tsx
+++ frontend/src/components/features/Search.tsx
@@ -4,14 +4,14 @@ export const Search = async (
   episodeId: number | string
 ) => {
   const hitTranscripts = await fetch(
-    `http://localhost:8080/api/podcasts/${idx}/search?text=${searchText}&episodeId=${episodeId}`
+    `${process.env.NEXT_PUBLIC_APP_DOMAIN}/api/podcasts/${idx}/search?text=${searchText}&episodeId=${episodeId}`
   ).then((data) => data.json());
   return hitTranscripts;
 };
 
 export const FetchFullTranscription = async (episodeId: number) => {
   const transcripts = await fetch(
-    `http://localhost:8080/api/episodes/${episodeId}/fulltransciption`
+    `${process.env.NEXT_PUBLIC_APP_DOMAIN}/api/episodes/${episodeId}/fulltransciption`
   ).then((data) => data.json());
   return transcripts;
 };
